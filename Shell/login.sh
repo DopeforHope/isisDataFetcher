dc3a4827ea369ca059751b4765b103d93a7036f2
Xuij <Xuij@users.noreply.github.com>
2016-08-20 21:40:20 +0200

    Update login.sh
    Moodle 3.1 Update Bypass

--- Shell/login.sh
+++ Shell/login.sh
@@ -5,6 +5,15 @@ clear
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+if [ ! -d "Cookie" ]; then
+mkdir Cookie
+fi
+
+if [ ! -d "Homepage" ]; then
+mkdir Homepage
+fi
+
+
 #get column rows to display title of the programm in the middle [fancy.sh]
 COLUMNS=$(tput cols) 
 title="~Shell Login Script~" 
@@ -17,15 +26,17 @@ read -s -p "Enter passwd: " passwd
 echo
 printf 'login you in...'
 #log in
-redirectedURL=$(curl $1 -s -L -I -b Cookies/keks.jar -c Cookies/keks.jar -o /dev/null -w '%{url_effective}' https://isis.tu-berlin.de/auth/shibboleth/index.php)
+redirectedURL=$(curl $1 -s -L -I -b Cookie/keks.jar -c Cookie/keks.jar -o /dev/null -w '%{url_effective}' https://isis.tu-berlin.de/auth/shibboleth/index.php)
 echo -ne '...'
-secondRedirectURL=$(curl $1 -s -L -b Cookies/keks.jar -c Cookies/keks.jar -w '%{url_effective}' -d "'shib_idp_ls_exception.shib_idp_session_ss=&shib_idp_ls_success.shib_idp_session_ss=false&shib_idp_ls_value.shib_idp_session_ss=&shib_idp_ls_exception.shib_idp_persistent_ss=&shib_idp_ls_success.shib_idp_persistent_ss=false&shib_idp_ls_value.shib_idp_persistent_ss=&shib_idp_ls_supported=&_eventId_proceed='" -o /dev/null $redirectedURL )
+secondRedirectURL=$(curl $1 -s -L -b Cookie/keks.jar -c Cookie/keks.jar -w '%{url_effective}' -d "'shib_idp_ls_exception.shib_idp_session_ss=&shib_idp_ls_success.shib_idp_session_ss=false&shib_idp_ls_value.shib_idp_session_ss=&shib_idp_ls_exception.shib_idp_persistent_ss=&shib_idp_ls_success.shib_idp_persistent_ss=false&shib_idp_ls_value.shib_idp_persistent_ss=&shib_idp_ls_supported=&_eventId_proceed='" -o /dev/null $redirectedURL )
 echo -ne '...'
-curl -s -L -b Cookies/keks.jar -c Cookies/keks.jar --data 'j_username=$username&j_password=$passwd&_eventId_proceed=' $secondRedirectURL -o Homepage/InformationSystemforStudents.html
+curl -s -L -b Cookie/keks.jar -c Cookie/keks.jar -d "j_username=$username&j_password=$passwd&_eventId_proceed=" $secondRedirectURL -o Homepage/InformationSystemforStudents.html
+#IMPORTANT CURL NEEDS -d " DOUBLE QUOTES TO TRANSMIT VARIABLES, SINGLE QUOTE IS FOR SUCKERS"
 echo -ne '...'
 echo
-benutzerName=$(grep -o 'Profil anzeigen">.*</a>' homepage.html | cut -f2- -d'>' | cut -f1 -d'<' )
+benutzerName=$(grep -o 'Profil anzeigen">.*</a>' Homepage/InformationSystemforStudents.html | cut -f2- -d'>' | cut -f1 -d'<' )
 echo Sie sind angemeldet als ${bold}${benutzerName}${normal}
 echo Ihre ISIS seite befindet sich im Ordner ${bold}Homepage/${normal}
 echo viel Spaß!
+#cd Homepage/ && xdg-open InformationSystemforStudents.html 
 echo
