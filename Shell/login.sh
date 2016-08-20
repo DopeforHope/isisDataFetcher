136a2cf83f94b96876272e072070bbf70a86a7b7
Xuij <Xuij@users.noreply.github.com>
2016-08-20 20:31:02 +0200

    Update login.sh

--- Shell/login.sh
+++ Shell/login.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
 clear
 
+#bold text
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 #get column rows to display title of the programm in the middle [fancy.sh]
 COLUMNS=$(tput cols) 
-title="+++++~Shell Login Script~+++++" 
+title="~Shell Login Script~" 
 printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
 #[/fancy.sh]
-
-# check if the dire
-
-if [ ! -d "cookieJar" ]; then
-  echo a first time for everything!
-fi
-
-if [ -d "cookieJar" ]; then
-  # Shit there is no goto statement in bash, i'll probably separate this in another bash script and load the separeted scripts indidually
- #should go in here: curl -L -b cookieJar/isis.jar https://isis.tu-berlin.de/my/ --progress-bar -o homepage.html
-
- echo you have done this before, havent you?
-fi
 
 read -p "Enter hostname: " username
 read -s -p "Enter passwd: " passwd
 # -s command silences the input of the passwd (you can also display asterisk but need improvement)
 echo
-echo loggin you in...
-
+printf 'login you in...'
 #log in
-#need a variable setting for cookieJar/isis.jar (too long, to type)
-curl -L -s -b cookieJar/isis.jar -c cookieJar/isis.jar https://isis.tu-berlin.de/auth/shibboleth/index.php -o silence
-#steal the cookie
-
-curl -L -s -b cookieJar/isis.jar -c cookieJar/isis.jar -d "j_username=$username&j_password=$passwd&Submit=Login" https://shibboleth.tubit.tu-berlin.de/idp/Authn/UserPassword -o silence
-#gives login data with stolen cookie to make shibboleth happy
-
-curl -L -b cookieJar/isis.jar https://isis.tu-berlin.de/my/ -o homepage.html
-#to enter isis directly with cookie.
-
-rm silence
-#because it's just data
-echo game over
-
-
-
-
-
-#we have now turned shit into gold.
+redirectedURL=$(curl $1 -s -L -I -b Cookies/keks.jar -c Cookies/keks.jar -o /dev/null -w '%{url_effective}' https://isis.tu-berlin.de/auth/shibboleth/index.php)
+echo -ne '...'
+secondRedirectURL=$(curl $1 -s -L -b Cookies/keks.jar -c Cookies/keks.jar -w '%{url_effective}' -d "'shib_idp_ls_exception.shib_idp_session_ss=&shib_idp_ls_success.shib_idp_session_ss=false&shib_idp_ls_value.shib_idp_session_ss=&shib_idp_ls_exception.shib_idp_persistent_ss=&shib_idp_ls_success.shib_idp_persistent_ss=false&shib_idp_ls_value.shib_idp_persistent_ss=&shib_idp_ls_supported=&_eventId_proceed='" -o /dev/null $redirectedURL )
+echo -ne '...'
+curl -s -L -b Cookies/keks.jar -c Cookies/keks.jar --data 'j_username=$username&j_password=$passwd&_eventId_proceed=' $secondRedirectURL -o Homepage/InformationSystemforStudents.html
+echo -ne '...'
+echo
+benutzerName=$(grep -o 'Profil anzeigen">.*</a>' homepage.html | cut -f2- -d'>' | cut -f1 -d'<' )
+echo Sie sind angemeldet als ${bold}${benutzerName}${normal}
+echo Ihre ISIS seite befindet sich im Ordner ${bold}Homepage/${normal}
+echo viel Spaß!
+echo
